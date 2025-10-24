ab636759ce4e0e22519def1f2036e4f1e7ef2dd8
thong-6 <30thang5nam2021@gmail.com>
2025-10-24 16:54:44 +0700

    message

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world thong");
 })
 app.listen(PORT, () => {
     console.log(`My app is running on port ${PORT}`)
