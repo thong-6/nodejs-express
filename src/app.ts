20326834df42c464e65568a04c3ba6bd4d486888
thong-6 <30thang5nam2021@gmail.com>
2025-10-24 18:13:00 +0700

    message

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-// const express = require("express");
+
 import express from "express";
 import 'dotenv/config'
 const app = express();
 const PORT = process.env.PORT || 8080;
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
-    res.send("Hello world thong");
+    res.render('home.ejs');
 })
 app.listen(PORT, () => {
     console.log(`My app is running on port ${PORT}`);
-    console.log(`env port ${process.env.PORT}`)
+
 })
