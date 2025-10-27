61f7c1eea7897e6ef00553c8eb7133a3afcf689b
thong-6 <30thang5nam2021@gmail.com>
2025-10-27 09:01:43 +0700

    message

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -5,5 +5,9 @@ const getHomePage = (req: Request, res: Response) => {
 const getCreateNewUser = (req: Request, res: Response) => {
     return res.render('create-user.ejs')
 }
+const postCreateNewUser = (req: Request, res: Response) => {
+    console.log(req.body);
+    return res.redirect('/');
+}
 
-export { getHomePage, getCreateNewUser }
+export { getHomePage, getCreateNewUser, postCreateNewUser }
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,10 +1,11 @@
 import express, { Express } from "express";
-import { getCreateNewUser, getHomePage } from "../controllers/user.controllers";
+import { getCreateNewUser, getHomePage, postCreateNewUser } from "../controllers/user.controllers";
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
     router.get('/', getHomePage)
-    router.get('/create-user', getCreateNewUser)
+    router.get('/create-user', getCreateNewUser);
+    router.post('/create-user', postCreateNewUser);
     app.use('/', router);
 }
 export default webRoutes;
